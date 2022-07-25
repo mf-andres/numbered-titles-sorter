97017ad3033a46771f82bde3a31edd3683a4cd70
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2022-07-25 14:20:48 +0200

    chore: advance on refactoring, work on subtitle processing

--- src/numbered_titles_sorter.rs
+++ src/numbered_titles_sorter.rs
@@ -4,18 +4,16 @@ use regex::Regex;
 
 pub fn sort_numbered_titles(file_contents: &str) -> String {
     // Extract lines and number of lines from file_contents
-    let file_lines: Vec<String> = file_contents.lines().map(|x| String::from(x)).collect();
+    let mut file_lines: Vec<String> = file_contents.lines().map(|x| String::from(x)).collect();
     let number_of_lines = file_lines.len();
 
     // Search for all title and subtitle positions
     let title_positions: Vec<usize> = get_title_positions(file_contents, 0);
     let subtitle_positions: Vec<usize> = get_title_positions(file_contents, 1);
 
-    /*
     //depth 1
-    // TODO title positions can be used as parameter of correct_titles function to not iterate over the whole file again
     let number_of_titles: usize = title_positions.len();
-    let mut processed_file_contents = correct_titles(file_contents, number_of_titles);
+    correct_titles(&mut file_lines, &title_positions, number_of_titles);
 
     // depth 2
     let mut title_numbers_range = 1..number_of_titles + 1;
@@ -25,29 +23,26 @@ pub fn sort_numbered_titles(file_contents: &str) -> String {
     {
         let title_number = title_numbers_range.next().unwrap();
         let subtitle_positions_between_titles: Vec<usize> = get_subtitle_positions_between_titles(
-            file_contents,
+            &subtitle_positions,
             previous_title_position,
             next_title_position,
         );
         let number_of_subtitles_between_titles: usize = subtitle_positions_between_titles.len();
-        processed_file_contents = correct_subtitles_between_titles(
-            file_contents,
+        correct_subtitles_between_titles(
+            &mut file_lines,
             title_number,
-            subtitle_positions_between_titles,
+            &subtitle_positions_between_titles,
             number_of_subtitles_between_titles,
         )
     }
-
-    println!("{}", processed_file_contents);
-    */
     let processed_file_contents = String::from("");
     processed_file_contents
 }
 
 fn correct_subtitles_between_titles(
-    file_contents: &str,
+    file_lines: &mut Vec<String>,
     title_number: usize,
-    subtitle_positions_between_titles: Vec<usize>,
+    subtitle_positions_between_titles: &Vec<usize>,
     number_of_subtitles_between_titles: usize,
 ) -> String {
     let mut processed_file_contents = String::from("");
@@ -92,7 +87,6 @@ fn get_title_positions(file_contents: &str, title_depth: u64) -> Vec<usize> {
             title_positions.push(line_number);
         }
     }
-    println!("title positions: {:?}", title_positions);
     title_positions
 }
 
@@ -111,52 +105,43 @@ fn get_title_pattern(title_depth: u64) -> String {
         title_pattern.push_str(pattern_extension);
     }
     title_pattern.push_str(" ");
-    println!("pattern {}", &title_pattern);
     title_pattern
 }
 
-fn process_line(line: &str, line_number: usize) -> String {
-    let new_title = format!("{} ", line_number.to_string());
+fn correct_titles(
+    file_lines: &mut Vec<String>,
+    title_positions: &Vec<usize>,
+    number_of_titles: usize,
+) {
+    let mut title_number_range = 1..number_of_titles + 1;
+    for title_position in title_positions {
+        let title_number = title_number_range.next().unwrap();
+        let line = &file_lines[*title_position];
+        let processed_line = process_line(line, title_number);
+        file_lines[*title_position] = processed_line;
+    }
+}
+
+// TODO generalize with depth
+fn process_line(line: &String, line_number: usize) -> String {
+    let new_title = format!("{}. ", line_number.to_string());
     let re = Regex::new(r"^\d\. ").unwrap();
-    let processed_line = re.replace(line, new_title).to_string();
+    let processed_line = re.replace(&line, new_title).to_string();
     processed_line
 }
 
-fn correct_titles(file_contents: &str, number_of_titles: usize) -> String {
-    let mut processed_file_contents = String::from("");
-    let mut title_number_range = 1..number_of_titles + 1;
-    for line in file_contents.lines() {
-        let processed_line;
-        if has_title(line, 0) {
-            let title_number = title_number_range.next().unwrap();
-            processed_line = process_line(line, title_number);
-        } else {
-            processed_line = line.to_string();
-        }
-        processed_file_contents = format!("{}{}\n", processed_file_contents, processed_line);
-    }
-    processed_file_contents
-}
-
 fn get_subtitle_positions_between_titles(
-    file_contents: &str,
+    subtitle_positions: &Vec<usize>,
     previous_title_position: usize,
     next_title_position: usize,
 ) -> Vec<usize> {
-    let mut subtitle_positions: Vec<usize> = vec![];
-    let lines_enumeration = file_contents
-        .lines()
-        .enumerate()
-        .filter(|(line_number, line)| {
-            previous_title_position > *line_number && *line_number < next_title_position
-        });
-    for (line_number, line) in lines_enumeration {
-        if has_subtitle(line) {
-            subtitle_positions.push(line_number);
-        }
-    }
-    println!("subtitle positions {:?}", &subtitle_positions);
-    subtitle_positions
+    let subtitle_positions_between_titles: Vec<usize> = subtitle_positions
+        .iter()
+        .filter(|x| previous_title_position < **x && **x < next_title_position)
+        .map(|x| *x)
+        .collect();
+    println!("{:?}", subtitle_positions_between_titles);
+    subtitle_positions_between_titles
 }
 
 fn has_subtitle(line: &str) -> bool {
