19d87301e7595ff4e6115d0dce4ca961976cf216
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2022-07-27 15:11:21 +0200

    feat: read file name from cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod file_reader;
-mod numbered_titles_sorter;
 mod file_writer;
+mod numbered_titles_sorter;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(name = "numbered_titles_sorter")]
+#[clap(author = "MF Andrés <munozfernandezandres@gmail.com>")]
+#[clap(version = "0.1")]
+#[clap(about = "Sorts/updates title numbers", long_about = None)]
+struct Cli {
+    #[clap(value_parser)]
+    file: String,
+}
 
 fn main() {
+    let cli = Cli::parse();
+    println!("{:?}", cli);
+
     let file_contents = file_reader::read_file();
     let processed_file_contents = numbered_titles_sorter::sort_numbered_titles(&file_contents);
     file_writer::write_file(&processed_file_contents);
