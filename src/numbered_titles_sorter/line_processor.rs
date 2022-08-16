4329872c956cbe98956e29ed14bee26a00316103
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2022-08-16 11:37:15 +0200

    refactor: removed unused method

--- src/numbered_titles_sorter/line_processor.rs
+++ src/numbered_titles_sorter/line_processor.rs
@@ -9,13 +9,3 @@ pub(crate) fn process_line(line: &String, title_depth: u32, title_number: &Strin
     let processed_line = re.replace(&line, title_number).to_string();
     processed_line
 }
-
-pub(crate) fn title_from(title_numbers: Vec<usize>) -> String {
-    let mut title = String::from("");
-    for title_number in title_numbers {
-        let title_fragment = format!("{}.", title_number.to_string());
-        title.push_str(&title_fragment);
-    }
-    title.push_str(" ");
-    title
-}
